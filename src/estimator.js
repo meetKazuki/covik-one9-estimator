57e065057d5babf4e265c7a97c3a604b2a584f71
Desmond Edem <edemintech@gmail.com>
2020-04-13 13:45:38 +0100

    setup assessment branch

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+import { estimateDays, getcurrentlyInfected, getInfectionsByDay } from './helpers';
+
+const covid19ImpactEstimator = (data) => {
+  const {
+    region,
+    periodType,
+    reportedCases,
+    timeToElapse,
+    totalHospitalBeds
+  } = data;
+
+  const currentlyInfected = getcurrentlyInfected(reportedCases);
+  const severelyInfected = getcurrentlyInfected(reportedCases, true);
+
+  const infectionsByTime = getInfectionsByDay();
+
+  return {
+    data,
+    impact: '',
+    severeImpact: ''
+  };
+};
 
 export default covid19ImpactEstimator;
